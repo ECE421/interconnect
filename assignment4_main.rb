26fd17917f53a79fbf9eaf9365bde2bbe16d502a
Thomas Lorincz <tlorincz@ualberta.ca>
2019-04-11 15:28:07 -0600

    Multiple instances of the app can now run concurrently

--- assignment4_main.rb
+++ assignment4_main.rb
@@ -3,7 +3,7 @@ require_relative 'src/app_model'
 require_relative 'src/app_presenter'
 
 # GUI Version
-app = Gtk::Application.new('interconnect4.com', :flags_none)
+app = Gtk::Application.new("interconnect4-#{Time.now.to_i}.com", :flags_none)
 model = AppModel.new(app, AppPresenter.new, AppModel::GUI)
 if ARGV.include?('cli')
   AppModel.new(nil, AppPresenter.new, AppModel::CLI) # CLI Version
